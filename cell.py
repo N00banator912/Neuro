89c06113e263f6d0cc16bae16502a06c860b4c20
Kat Brown <xxN00banator912xx@gmail.com>
2025-10-28 16:21:42 -0400

    Implementation Progress on Cells

--- cell.py
+++ cell.py
@@ -5,10 +5,35 @@
 
 # Congrations, Individual Cells are now complex enough that they warrant an entire file
 
-# So, let's design a new cell structure that will be beneficial later on.
+# Let's design a new cell structure that will be beneficial later on.
 # Cell:
 
-#	type
+#	type: land / water
+#   contents:   Agent, Food, Water, etc.
 #	name
-#	flavor(s)
-#	nutrition points (np) = experience points (xp) ~= kCal
+#   clarity
+#   walkable, swimable
+
+
+class Cell:
+    CELL_TYPE = [
+        'LAND',
+        'WATER']
+        
+    CONT_TYPE = [
+        'EMPTY',
+        'FOOD',
+        'AGENT',
+        'TERRAIN']
+        
+    def __init__(self, name="Cell", cType=CELL_TYPE[0], contents=None, clarity = 5, passable=True):
+        self.name = name
+        self.cType = cType
+        self.contents = contents
+        
+        # If the cell is not empty, do anything else that needs done
+        if self.contents != None:
+            return -1
+        
+        self.clarity = clarity
+        self.passable = passable
